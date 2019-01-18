de4763069fb9a43f3ce82d80464c488e477b854a
Grady Robbins <gradyrobbins13@gmail.com>
2019-01-17 20:03:49 -0600

    last commit of the day

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,19 @@ console.log("Hello main.js")
 import dataManager from "./dataManager";
 import eventListeners2 from "./eventListeners2"
 // import form from "./form"
-import interestsList from "./interestsList"
+// import interestsList from "./interestsList"
+import domBuilder from "./domBuilder"
 
 
 eventListeners2.renderForm();
-interestsList.newsify()
+// interestsList.newsify()
+domBuilder.appendInterests();
+domBuilder.appendPlaces();
 
-dataManager.getAllPlaces();
+dataManager.getAllPlaces()
+.then(response => console.log("getAllPlaces" , response))
+
+
 dataManager.getAllInterests();
 
 
