4a5bfbcccca0438fe5329780b675f366083fe6b5
Grady Robbins <gradyrobbins13@gmail.com>
2019-01-18 08:28:55 -0600

    add while loop to remove duplicate appended children when appending via interestList.newsify()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,22 +4,22 @@ console.log("Hello main.js")
 import dataManager from "./dataManager";
 import eventListeners2 from "./eventListeners2"
 // import form from "./form"
-// import interestsList from "./interestsList"
+import interestsList from "./interestsList"
 import domBuilder from "./domBuilder"
 
 
 eventListeners2.renderForm();
-// interestsList.newsify()
 domBuilder.appendInterests();
 domBuilder.appendPlaces();
 
 dataManager.getAllPlaces()
 // .then(response =>
-    // console.log("getAllPlaces" , response)
-    // )
+// console.log("getAllPlaces" , response)
+// )
 
 
 dataManager.getAllInterests();
+interestsList.newsify()
 
 
 
